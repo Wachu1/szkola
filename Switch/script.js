e13a0485826135151e9af5e5d71d0c54ec2055a8
Wachu1 <dominik.wach06@gmail.com>
2024-01-28 03:20:37 +0100

    Wersja zapasowa

--- Switch/script.js
+++ Switch/script.js
@@ -1,11 +1,13 @@
 const date = new Date();
 let hour = date.getHours();
 let day = date.getDay();
-let name = prompt("Proszę podaj imię");
+let user = prompt("Proszę podaj imię");
 
 if (hour < 12) {
-  document.querySelector("#hour").innerHTML = '<h4>" Dzień dobry " + name </h4>';
+  document.querySelector("#hour").innerHTML = `<h4> Dzień dobry ${user} </h4>`;
 }else if ((hour >= 12) && (hour < 18)){
-    document.querySelector("#hour").innerHTML = '<h4>"Miłego popołudnia" + name </h4>';
-}else if
+    document.querySelector("#hour").innerHTML = `<h4> Miłego popołudna ${user} </h4>`;
+}else if ((hour >= 18) && (hour < 24)){
+  document.querySelector("#hour").innerHTML = `<h4> Dobry wieczór ${user} </h4>`;
+}
 
