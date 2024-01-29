ad35f18017fb9e8a839376d09bccdfe30a7e399b
Wachu1 <dominik.wach06@gmail.com>
2024-01-29 19:39:17 +0100

    Wersja kolejna z poprawkami do programu

--- Switch/script.js
+++ Switch/script.js
@@ -11,3 +11,36 @@ if (hour < 12) {
   document.querySelector("#hour").innerHTML = `<h4> Dobry wieczór ${user} </h4>`;
 }
 
+//let dayList = ["Niedziela", "poniedziałek", "wtorek", "sroda", ];
+
+switch (day){
+  case 0:
+    document.querySelector("#day").innerHTML = `<b>Dziś jest niedziela </b>`;
+    break;
+  case 1:
+    document.querySelector("#day").innerHTML = `<b>Dziś jest poniedziałek </b>`;
+    break;
+  case 2:
+    document.querySelector("#day").innerHTML = `<b>Dziś jest wtorek </b>`;
+    break;
+  case 3:
+    document.querySelector("#day").innerHTML = `<b>Dziś jest środa </b>`;
+    break;
+  case 4:
+    document.querySelector("#day").innerHTML = `<b>Dziś jest czwartek </b>`;
+    break;
+  case 5:
+    document.querySelector("#day").innerHTML = `<b>Dziś jest piątek </b>`;
+    break;
+  case 6:
+    document.querySelector("#day").innerHTML = `<b>Dziś jest sobota </b>`;
+    break;
+  default:
+      console.log("Przykro mi niepoprawy dzien tygodnia");
+}
+
+
+
+
+
+
